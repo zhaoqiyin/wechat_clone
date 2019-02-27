f945f51738d0dc0f21206bec8e2402be98eea0fa
yinzhaoqi <yinzhaoqi@126.com>
2019-02-27 15:43:44 +0800

    改变底部tab icon的颜色

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../constants.dart' show Constants;
+import '../constants.dart' show Constants, AppColors;
 
 class NavigationIconView {
   final String _title;
@@ -13,9 +13,12 @@ class NavigationIconView {
     _icon = icon,
     _activeIcon = activeIcon,
     item = BottomNavigationBarItem(
-      icon: Icon(icon),
-      activeIcon: Icon(activeIcon),
-      title: Text(title),
+      icon: Icon(icon, color: Color(AppColors.TabIconNormal)),
+      activeIcon: Icon(activeIcon, color: Color(AppColors.TabIconActive)),
+      title: Text(title, style: TextStyle(
+        fontSize: 14.0,
+        color: Color(AppColors.TabIconNormal)
+      ),),
       backgroundColor: Colors.white
     );
 }
